bef35f4df156ed3579dfe40bf3db965ac6aa208b
Navaneeth-PV <navaneethpv550@gmail.com>
2025-12-28 10:01:49 +0530

    feat: add Clerk-based sign-in page

--- web/src/app/(pages)/sign-in/[[...sign-in]]/page.tsx
+++ web/src/app/(pages)/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/app/(pages)/components/Navbar";
 export default function Page() {
   return (
     <div className="min-h-screen bg-white">
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="flex items-center justify-center py-20">
         <SignIn />
       </div>
